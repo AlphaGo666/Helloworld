f3ff1437016213fb215c34b6eb6db971aa8f1664
AlphaGo666 <AlphaGo666@163.com>
2016-11-03 14:49:55 +0800

    Initial commit

---
+++ src/com/neocross/cn/Test.java
@@ -0,0 +1,12 @@
+package com.neocross.cn;
+
+/**
+ * Created by Shining on 2016/11/1.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.print("test");
+        System.out.println();
+    }
+
+}
